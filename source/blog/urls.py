0125212e1013cb5d1b1868fcd8e322338525a821
Farid <thesomeq@gmail.com>
2019-09-10 19:59:29 +0600

    Простое удаление.

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, article_view, article_create_view, article_update_view
+from webapp.views import index_view, article_view, article_create_view, article_update_view, \
+    article_delete_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('article/<int:pk>/', article_view, name='article_view'),
     path('article/add/', article_create_view, name='article_add'),
     path('article/<int:pk>/update/', article_update_view, name='article_update'),
+    path('article/<int:pk>/delete/', article_delete_view, name='article_delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -37,3 +37,9 @@ def article_update_view(request, pk):
         article.text = request.POST.get('text')
         article.save()
         return redirect('article_view', pk=article.pk)
+
+
+def article_delete_view(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('index')
