a6e9913d233759de21181c1aa25d039feb08c65d
Farid <thesomeq@gmail.com>
2019-09-10 22:56:49 +0600

    Создание статьи с помощью формы.

---
+++ source/webapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import widgets
+
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(max_length=200, label='Title', required=True)
+    author = forms.CharField(max_length=40, label='Author', required=True)
+    text = forms.CharField(max_length=3000, label='Text', required=True,
+                           widget=widgets.Textarea)
