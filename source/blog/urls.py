bac23f326d95870e00a82a7ac04066682a989382
Farid <thesomeq@gmail.com>
2019-09-27 21:02:06 +0600

    Пример представления для добавления комментариев.

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import IndexView, ArticleView, ArticleCreateView, ArticleUpdateView, \
-    ArticleDeleteView
+from webapp.views import IndexView, ArticleView, ArticleCreateView, \
+    ArticleUpdateView, ArticleDeleteView, CommentCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('article/<int:pk>/', ArticleView.as_view(), name='article_view'),
     path('article/add/', ArticleCreateView.as_view(), name='article_add'),
-    path('article/<int:pk>/update/', ArticleUpdateView, name='article_update'),
-    path('article/<int:pk>/delete/', ArticleDeleteView, name='article_delete'),
+    path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('comment/add/', CommentCreateView.as_view(), name='comment_add')
 ]
