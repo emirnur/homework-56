382497211c608f814a972ae91707f453be05b7c0
Farid <thesomeq@gmail.com>
2019-10-18 21:32:04 +0600

    Начало поиска - поиск по тексту, форма поиска + валидация для него.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -49,3 +49,30 @@ class ArticleCommentForm(forms.ModelForm):
 
 class SimpleSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label='Найти')
+
+
+class FullSearchForm(forms.Form):
+    text = forms.CharField(max_length=100, required=False, label='Текст')
+    in_title = forms.BooleanField(initial=True, required=False, label='В заголовках')
+    in_text = forms.BooleanField(initial=True, required=False, label='В тексте')
+    in_tags = forms.BooleanField(initial=True, required=False, label='В тегах')
+    in_comment_text = forms.BooleanField(initial=False, required=False, label='В тексте комментариев')
+
+    author = forms.CharField(max_length=100, required=False, label='Автор')
+    in_articles = forms.BooleanField(initial=True, required=False, label='Статей')
+    in_comments = forms.BooleanField(initial=False, required=False, label='Комментариев')
+
+    def clean(self):
+        super().clean()
+        text = self.cleaned_data.get('text')
+        in_title = self.cleaned_data.get('in_title')
+        in_text = self.cleaned_data.get('in_text')
+        in_tags = self.cleaned_data.get('in_tags')
+        in_comment_text = self.cleaned_data.get('in_comment_text')
+        if text:
+            if not (in_title or in_text or in_tags or in_comment_text):
+                raise ValidationError(
+                    'One of the checkboxes: In Title, In Text, In Tags, In Comment text should be checked.',
+                    code='no_text_search_destination'
+                )
+        return self.cleaned_data
--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -3,9 +3,9 @@ from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from django.utils.http import urlencode
 from django.views.generic import ListView, DetailView, CreateView,\
-    UpdateView, DeleteView
+    UpdateView, DeleteView, FormView
 
-from webapp.forms import ArticleForm, ArticleCommentForm, SimpleSearchForm
+from webapp.forms import ArticleForm, ArticleCommentForm, SimpleSearchForm, FullSearchForm
 from webapp.models import Article, STATUS_ARCHIVED, STATUS_ACTIVE
 from django.core.paginator import Paginator
 
@@ -105,3 +105,32 @@ class ArticleDeleteView(DeleteView):
         self.object.status = STATUS_ARCHIVED
         self.object.save()
         return redirect(self.get_success_url())
+
+
+class ArticleSearchView(FormView):
+    template_name = 'article/search.html'
+    form_class = FullSearchForm
+
+    def form_valid(self, form):
+        text = form.cleaned_data.get('text')
+        query = self.get_text_search_query(form, text)
+        context = self.get_context_data(form=form)
+        context['articles'] = Article.objects.filter(query).distinct()
+        return self.render_to_response(context=context)
+
+    def get_text_search_query(self, form, text):
+        query = Q()
+        if text:
+            in_title = form.cleaned_data.get('in_title')
+            if in_title:
+                query = query | Q(title__icontains=text)
+            in_text = form.cleaned_data.get('in_text')
+            if in_text:
+                query = query | Q(text__icontains=text)
+            in_tags = form.cleaned_data.get('in_tags')
+            if in_tags:
+                query = query | Q(tags__name__iexact=text)
+            in_comment_text = form.cleaned_data.get('in_comment_text')
+            if in_comment_text:
+                query = query | Q(comments__text__icontains=text)
+        return query
